8b737f1c54babc48724facc823eaaec514c92726
Ivan <Zolotukhin.ivan.1@gmail.com>
2025-06-06 00:59:53 +0200

    start project in my shop

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Feferon
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://feferon.cz/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Jsme jedineční a nejlepší
         </a>
       </header>
     </div>
